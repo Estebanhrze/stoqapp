d6e61f13e962ebc9b8abad0dcd6700b411b411a3
Esteban Hernandez <hectoresteban6972@gmail.com>
2025-10-22 19:55:11 -0500

    providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Spatie\Activitylog\Models\Activity;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
-    {
-        //
-    }
+{
+    Activity::saving(function (Activity $activity) {
+        // agrega IP y user agent a cualquier log (automático o manual)
+        $props = collect($activity->properties ?? []);
+        $activity->properties = $props->merge([
+            'ip' => request()->ip(),
+            'user_agent' => request()->userAgent(),
+        ]);
+    });
+}
 }
