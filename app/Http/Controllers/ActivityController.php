a94dcc8d9a7ce7546ca8d319b3070b2d5e2f4d78
dargudo123 <dargudo123@gmail.com>
2025-10-23 15:58:19 -0500

    Cambios backend 2

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -22,13 +22,6 @@ class ActivityController extends Controller
         });
     }
 
-    // Si luego quieres filtrar por modelo o log_name, puedes reactivar estas:
-    /*
-    if ($r->filled('model'))   $q->where('subject_type', $r->input('model'));
-    if ($r->filled('subject')) $q->where('subject_id', (int)$r->input('subject'));
-    if ($r->filled('log'))     $q->where('log_name', $r->input('log'));
-    */
-
     $logs = $q->paginate(20)->withQueryString();
 
     return view('activity.index', compact('logs'));
